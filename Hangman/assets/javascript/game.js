8ae96bda17db945947a5e80d3510515b053d7a0b
davidb9790 <davidbermudez97@gmail.com>
2016-05-28 18:44:37 -0400

    almost complete

---
+++ Hangman/assets/javascript/game.js
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Game</title>
+</head>
+<body>
+<script>
+
+	console.log("Hello World!");
+	characters=['goku','piccolo','yamcha','vegeta','krillin','tien','gohan']
+
+	var alphabet =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q',
+	'r','s','t','u','v','w','x','y','z']
+
+	var characters;// array of topics
+	var word;// selected word to guess
+	var guess;//guess
+	var guesses =[]; //stored guesses
+	var lives;
+	var counter;
+	var space; //spaces in the word
+	var wins=0;
+	var length = characters.length;
+	
+</script>
+
+</body>
+</html>
